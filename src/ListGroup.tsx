19bc19de1fc72a9c8cc29ec53f6565ef49d946c3
samrat53 <samrat05hccs@gmail.com>
2023-07-31 01:26:58 +0530

    second commit-day1

--- src/ListGroup.tsx
+++ src/ListGroup.tsx
@@ -1,21 +1,54 @@
-function ListGroup(){
-    let items=["Cse","ece","ee","eie","ce","mech","produc","bio","chem"];
-    items=[];
+import { MouseEvent } from "react";
 
-    //1. const getList=()=> (items.length===0) ? (<p>No list found</p>):(null); 
-    //single line statement in function doesnot need return keyword
+function ListGroup() {
+  let items = [
+    "Cse",
+    "ece",
+    "ee",
+    "eie",
+    "ce",
+    "mech",
+    "produc",
+    "bio",
+    "chem",
+  ];
 
-    return (
+  const handleClick = (event: MouseEvent) => {
+    console.log(event);
+    // const button=event.target as HTMLButtonElement;
+    // console.log(button.innerHTML)};
+  };
+
+  // items=[];
+
+  //1. const getList=()=> (items.length===0) ? (<p>No list found</p>):(null);
+  //single line statement in function doesnot need return keyword
+
+  return (
     <>
-    <h1>List</h1>
-    <ul className="list-group">
-
+      <h1>List</h1>
+      <ul className="list-group">
         {/* 1. {getList()} */}
 
-        {items.length===0 && <p>No list found</p>} 
+        {items.length === 0 && <p>No list found</p>}
         {/* if the above statement is false then entire statement is false and hence nothing is rendered.
         jump to next line*/}
-        {items.map((values,index) => <li key={index+1}>{index+1}.{values}</li>)} 
+
+        {items.map((values, index) => (
+          <li className="list-group-item" key={values} onClick={handleClick}>
+            {index + 1}.{values}
+          </li>
+        ))}
+
+        {/*  2. {items.map((values, index) => (
+          <li
+            className="list-group-item"
+            key={index + 1}
+            onClick={(event) => console.log(`value: ${values} and index: ${index} and eventis ${event}`)}
+          >
+            {index + 1}.{values}
+          </li>
+        ))} */}
 
         {/* take each value from the array and convert each value to a list type.
         use curly braces  to render data dynamically */}
@@ -25,8 +58,8 @@ function ListGroup(){
         <li className="list-group-item">A third item</li>
         <li className="list-group-item">A fourth item</li>
         <li className="list-group-item">And a fifth one</li> */}
-    </ul>
+      </ul>
     </>
-    )
+  );
 }
 export default ListGroup;
