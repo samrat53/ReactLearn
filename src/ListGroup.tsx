be8b1446a6a7e23482baf95b8bf45d23524cef4e
samrat53 <samrat05hccs@gmail.com>
2023-07-31 00:36:05 +0530

    render types

--- src/ListGroup.tsx
+++ src/ListGroup.tsx
@@ -1,12 +1,32 @@
 function ListGroup(){
+    let items=["Cse","ece","ee","eie","ce","mech","produc","bio","chem"];
+    items=[];
+
+    //1. const getList=()=> (items.length===0) ? (<p>No list found</p>):(null); 
+    //single line statement in function doesnot need return keyword
+
     return (
+    <>
+    <h1>List</h1>
     <ul className="list-group">
-        <li className="list-group-item">An item</li>
+
+        {/* 1. {getList()} */}
+
+        {items.length===0 && <p>No list found</p>} 
+        {/* if the above statement is false then entire statement is false and hence nothing is rendered.
+        jump to next line*/}
+        {items.map((values,index) => <li key={index+1}>{index+1}.{values}</li>)} 
+
+        {/* take each value from the array and convert each value to a list type.
+        use curly braces  to render data dynamically */}
+
+        {/* <li className="list-group-item">An item</li>
         <li className="list-group-item">A second item</li>
         <li className="list-group-item">A third item</li>
         <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        <li className="list-group-item">And a fifth one</li> */}
     </ul>
+    </>
     )
 }
 export default ListGroup;
