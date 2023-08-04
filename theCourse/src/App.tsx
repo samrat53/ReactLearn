f921b05320fe877871f765315dae5be093354963
samrat53 <samrat05hccs@gmail.com>
2023-08-05 02:17:48 +0530

    add functionality

--- theCourse/src/App.tsx
+++ theCourse/src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 import UnOrderList from "./components/unolList";
 
 function App() {
-
-  const LuckyNo = 5;
+  let [LuckyNo, setLuckyNo] = useState(0);
+  for (let i = 0; i < 100; i++) {
+    setTimeout(() => {
+      setLuckyNo(i);
+      console.log(i);
+    }, i * 1000);
+    
+  }
+  // const LuckyNo = 5;
   // let currYear=new Date().getFullYear();
 
   return (
